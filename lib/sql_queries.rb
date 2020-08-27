a1652ea88f0e7853a45167854f42e586aa65fbda
Scott Wilson <scottwilson1020@gmail.com>
2020-08-27 01:00:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,11 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts
 end	
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT projects."
+  "SELECT user.name, SUM(pledges.amount)
+  FROM user
+  INNER JOIN 
+  
+  "
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
